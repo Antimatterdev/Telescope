09b2d267ec27b3815641a2e0f06bd00713bc8319
Antimatterdev <111754356+Antimatterdev@users.noreply.github.com>
2022-08-22 15:19:48 -0400

    Update events.js

--- Handlers/events.js
+++ Handlers/events.js
@@ -4,8 +4,8 @@ var table = new AsciiTable()
 table.setHeading('Events', 'Stats').setBorder('|', '=', "0", "0")
 
 module.exports = (client) => {
-    fs.readdirSync('./events/').filter((file) => file.endsWith('.js')).forEach((event) => {
-      	require(`../events/${event}`);
+    fs.readdirSync('./Events/').filter((file) => file.endsWith('.js')).forEach((event) => {
+      	require(`../Events/${event}`);
 	table.addRow(event.split('.js')[0], '✅')
     })
 	console.log(table.toString())
